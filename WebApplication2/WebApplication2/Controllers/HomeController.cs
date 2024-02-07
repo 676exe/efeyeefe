2414b8238721e6ff44cc03be1171dbb0d783cc22
eefee <124432670+676exe@users.noreply.github.com>
2024-02-07 21:34:59 +0300

    efe

---
+++ WebApplication2/WebApplication2/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class HomeController : Controller
+    {
+        UrunVt urunVt = new UrunVt();
+
+        public HomeController()
+        {
+           
+        }
+        public ActionResult Index()
+        {
+            //var model = _context.Tableİsmi.ToList();        tablo ismi gelecek  //
+            return View(); // Model ismi gelecek //
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication2/WebApplication2/Models/UrunBilgi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class UrunBilgi
+    {
+        [Key]
+        public int barkod { get; set; }
+        public string adi { get; set; }
+        public int birimGrami { get; set; }
+        public int birimFiyati { get; set; }
+        public int adet { get; set; }
+        public int resim { get; set; }
+        public int fire { get; set; }
+        public int koliAdeti { get; set; }
+        public int toplamFiyat { get; set; }
+        public int toplamGram { get; set; }
+    }
+
+    public class UrunVt: DbContext
+    {
+        public DbSet<UrunBilgi> UrunDb { get; set; }
+    }
+}
